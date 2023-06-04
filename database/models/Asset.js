e25d78f1c86c821a4ab6486d826086875e7cb6c1
Leo Cooper <113992401+defineprogramming@users.noreply.github.com>
2023-06-04 19:35:56 +0100

    Create Asset.js

---
+++ database/models/Asset.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require(‘sequelize’);
+const sequelize = require(‘…/config/database’);
+
+class Asset extends Model {}
+
+Asset.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: ‘Asset’,
+    timestamps: true,
+  }
+);
+
+module.exports = Asset;
